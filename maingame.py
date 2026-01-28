2f5564e58d53bad381314316c2964da09eb1c2ff
yasinashour-hub <yasin.ashour@long-view.com>
2026-01-28 15:43:32 -0600

    Got pygame working now on my home computer.

--- maingame.py
+++ maingame.py
@@ -1,5 +1,3 @@
-print("Hello World!")
-print("DID IT WORK?")
-#Okay guys. I followed advice from chatGPT and added this comment.
-#Let's see if it tracks changes properly now.
-pip install pygame
+import pygame
+pygame.init()
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
