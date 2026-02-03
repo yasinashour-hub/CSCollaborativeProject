e6bd665292851a86748d14f50fc760f23c2635a3
yasinashour-hub <yasin.ashour@long-view.com>
2026-02-02 21:17:21 -0600

    Did some basic setting up for the python and started the java file.

--- maingame.py
+++ maingame.py
@@ -1,3 +1,12 @@
+#Importing necessary libraries
 import pygame
+import time
+import random
 pygame.init()
 from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
+pygame.font.init()
+pygame.display.init()
+
+#Setting up display
+screen = pygame.display.set_mode((1000, 800))
+pygame.display.set_caption("Table Tennis")
