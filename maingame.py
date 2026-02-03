cd7e245c8cf0c9dc21b81e54aa4604395db166c5
hieuvlong-view <hieu.hoangvaillant@long-view.com>
2026-02-02 21:55:54 -0600

    Update maingame.py
    Hi

--- maingame.py
+++ maingame.py
@@ -10,3 +10,12 @@ pygame.display.init()
 #Setting up display
 screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Table Tennis")
+
+
+#trying to make simple figures for our tennis court
+#A blank canvas which I guess you need to make the imahge on
+TestSurface=pygame.surface((100,100), pygame.SRCALPHA)
+#how you draw a rectangle
+pygame.draw.rect(sprite_surface, (50,0,0),(0,0,25,25)
+#how to draw a circle
+pygame.draw.circle(sprite_surface, (50,0,0), (25), 25)
