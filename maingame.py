72fcc3809450dca152dc1b7473b0b4761eb40bff
yasinashour-hub <yasin.ashour@long-view.com>
2026-02-03 17:02:42 -0600

    Didn't change much

--- maingame.py
+++ maingame.py
@@ -10,12 +10,12 @@ pygame.display.init()
 #Setting up display
 screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Table Tennis")
-
+screen.fill((0,0,0))
 
 #trying to make simple figures for our tennis court
 #A blank canvas which I guess you need to make the imahge on
-TestSurface=pygame.surface((100,100), pygame.SRCALPHA)
+#TestSurface = pygame.surface((100,100), pygame.SRCALPHA)
 #how you draw a rectangle
-pygame.draw.rect(sprite_surface, (50,0,0),(0,0,25,25)
+#pygame.draw.rect(sprite_surface, (50,0,0),(0,0,25,25))
 #how to draw a circle
-pygame.draw.circle(sprite_surface, (50,0,0), (25), 25)
+#pygame.draw.circle(sprite_surface, (50,0,0), (25), 25)
