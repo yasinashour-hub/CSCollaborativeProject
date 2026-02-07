90f4eafd81e02647030ffb0926fb79ab4f2e1cb0
yasinashour-hub <yasin.ashour@long-view.com>
2026-02-07 13:59:13 -0600

    Finished Upgrade.java and put the first upgrade into ShopInventory.java

--- java/shop/logic/ShopInventory.java
+++ java/shop/logic/ShopInventory.java
@@ -1,14 +1,18 @@
-package shop.logic;
-// !!shop/logic is for rules and flow, not visuals
-// !!shop/logic/ShopInventory is for what upgrades the shop sells, has a list, methods to see what stuff we have,
-// contd... stuff for out of stock, tiers, and maybe sales
-// Item list: tiered shoes that give more speed, one-time use potions that give temp stat buffs for all of our stats, 
-// tiered rackets that give more power, different tennis balls to choose that increase diff stats, 
+package shop.logic; 
+import shop.model.Upgrade;
+// !!shop/logic is for rules and flow, not visuals 
+// !!shop/logic/ShopInventory is for what upgrades the shop sells, has a list, methods to see what stuff we have, 
+// contd... stuff for out of stock, tiers, and maybe sales 
+// Item list: tiered shoes that give more speed, one-time use potions that give temp stat buffs for all of our stats,
+// tiered rackets that give more power, different tennis balls to choose that increase diff stats,
 // a water bottle to increase a stamina stat for a match
 // and anything else you think of
-public class ShopInventory {
-    public static void main(String[] args) {
-       
-        }
-
-    }
+public class ShopInventory { 
+    public static void main(String[] args) { 
+        Upgrade shoes = new Upgrade( "Basic Tennis Shoes", 10, "Increases your base speed by 1.", 1);
+        System.out.println(shoes.getName()); 
+        System.out.println(shoes.getCost()); 
+        System.out.println(shoes.getDescription()); 
+        System.out.println(shoes.getBonusAmount()); 
+    } 
+}
--- java/shop/model/Upgrade.java
+++ java/shop/model/Upgrade.java
@@ -1,8 +1,31 @@
-package shop.model;
-// shop/model/ is where we will put the data and rules, not stuff w/ screens/clicks
-// shop/model/Upgrade.java is where the idea of the upgrades will be eg. cost, effect, name, description, etc.
-public class Upgrade {
-    public static void main(String[] args) {
-        
-    }
+package shop.model; 
+// !!THIS FILE IS DONE. DO NOT CODE ANYMORE HERE. 
+// shop/model/ is where we will put the data and rules, not stuff w/ screens/clicks 
+// shop/model/Upgrade.java is where the idea of the upgrades will be eg. cost, effect, name, description, etc. 
+public class Upgrade { 
+    // data for the upgrades 
+    private String name; 
+    private int cost; 
+    private String description; 
+    private int bonusAmount; 
+    // obvious 
+    public Upgrade(String name, int cost, String description, int bonusAmount) { 
+        this.name = name; 
+        this.cost = cost; 
+        this.description = description; 
+        this.bonusAmount = bonusAmount; 
+        } 
+    // getters for the data 
+    public String getName() { 
+        return name; 
+    } 
+    public int getCost() { 
+        return cost; 
+    } 
+    public String getDescription() { 
+        return description; 
+    } 
+    public int getBonusAmount() { 
+        return bonusAmount; 
+    } 
 }
