adb7edc3af819fcf8e30d8867ce21a95d169aa71
hieuvlong-view <hieu.hoangvaillant@long-view.com>
2026-02-07 12:53:16 -0600

    Update maingame.py
    Debugging

--- python/maingame.py
+++ python/maingame.py
@@ -12,45 +12,48 @@ velocity=0
 screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Table Tennis")
 screen.fill((0,0,0))
-acceleration=100
-velocity+=acceleration
+
 opponentscore=0
 playerscore=0
-velocity+=acceleration
+velocity = pygame.Vector2(0, 0)
+acceleration = pygame.Vector2(0, 100)
+velocity += acceleration
 position=+velocity
 #trying to make simple figures for our tennis court
 #A blank canvas which I guess you need to make the imahge on
 sprite_surface = pygame.surface((100,100), pygame.SRCALPHA)
 #how you draw a rectangle
-player=pygame.draw.circle(sprite_surface, (50,0,0),(25),50)
-opponent=pygame.draw.circle(sprite_surface, (-50,0,0),(25),50)
-#how to draw a circle
-circle=pygame.draw.circle(sprite_surface, (50,0,0), (25), 25)
-circle.x=0
-circle.y=0
-circle.x+=velocity.x 
-circle.y+=velocity.y
+player_rect=pygame.draw.rect(sprite_surface, (50,0,0),(25),50)
+opponent_rect=pygame.draw.rect(sprite_surface, (150,0,0),(25),50)
+#how to draw a ball
+ball_circle=pygame.draw.rect(sprite_surface, (50,0,0), (25), 25)
+ball_circle.x=0
+ball_circle.y=0
+ball_circle.x+=velocity.x 
+ball_circle.y+=velocity.y
 #physics
-acceleration=100
+
 
 position=+velocity
 
-if player.position==circle.position:
-    velocity.x=-velocity.x
-    velocity.y=-velocity.y
-circle.x+=velocity.x
-circle.y+=velocity.y
-if opponent.position==circle.position:
-    velocity.x=-velocity.x
-    velocity.y=-velocity.y
-circle.x+=velocity.x
-circle.y+=velocity.y
+
+
 def point():
-    if player.position<circle.position:
+    if player_rect.colliderect(ball_rect):
+        ball_circle.x-=velocity.x 
+        ball_circle.y-=velocity.y
+
+    if opponent_rect.colliderect(ball_rect):
+        ball_circle.x-=velocity.x 
+        ball_circle.y-=velocity.y
+    if player.centerx<ball.centerx:
+        global opponentscore, playerscore
         opponentscore=opponentscore+1
-        print("the opponent have",opponentscore)
+        print("the opponent has",opponentscore)
         print("you have", playerscore)
-    elif opponent.position>circle.position:
+    elif opponent.centerx>ball.centerx:
         playerscore=playerscore+1
-        print("the opponent have",opponentscore)
+        print("the opponent has",opponentscore)
         print("you have", playerscore)
+While True:
+    point()
