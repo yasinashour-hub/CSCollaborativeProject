7dd1a224838159f8dcd309e8e37d5e768c11ed64
yasinashour-hub <yasin.ashour@long-view.com>
2026-02-10 21:27:12 -0600

    Added another upgrade and reworked ShopInventory.java and Upgrade.java

--- java/shop/logic/ShopInventory.java
+++ java/shop/logic/ShopInventory.java
@@ -1,5 +1,7 @@
 package shop.logic; 
+
 import shop.model.Upgrade;
+
 // !!shop/logic is for rules and flow, not visuals 
 // !!shop/logic/ShopInventory is for what upgrades the shop sells, has a list, methods to see what stuff we have, 
 // contd... stuff for out of stock, tiers, and maybe sales 
@@ -7,12 +9,46 @@ import shop.model.Upgrade;
 // tiered rackets that give more power, different tennis balls to choose that increase diff stats,
 // a water bottle to increase a stamina stat for a match
 // and anything else you think of
+
 public class ShopInventory { 
     public static void main(String[] args) { 
-        Upgrade shoes = new Upgrade( "Basic Tennis Shoes", 10, "Increases your base speed by 1.", 1);
-        System.out.println(shoes.getName()); 
-        System.out.println(shoes.getCost()); 
-        System.out.println(shoes.getDescription()); 
-        System.out.println(shoes.getBonusAmount()); 
+
+        // Shoes upgrade
+        Upgrade shoes = new Upgrade( 
+            "Basic Tennis Shoes", 
+            10, 
+            "Increases your base speed by 1.",
+            "speed",
+            1, 
+            false,
+            false
+        );
+        
+        // Racket upgrade
+        Upgrade racket = new Upgrade(
+            "Basic Racket", 
+            10, 
+            "Increases your base power by 1.", 
+            "power",
+            1,
+            false,
+            false
+        );
+
+        // Potions
+        Upgrade speedPotion = new Upgrade(
+            "Speed Potion I", 
+            3, 
+            "Increases your speed by 5% for the next match.", 
+            "speed",
+            1.05,
+            true,
+            true
+        );
+
+
+        System.out.println(shoes.getName());
+        System.out.println(speedPotion.getDescription());
+        System.out.println(racket.getCost());
     } 
 }
--- java/shop/model/Upgrade.java
+++ java/shop/model/Upgrade.java
@@ -2,30 +2,62 @@ package shop.model;
 // !!THIS FILE IS DONE. DO NOT CODE ANYMORE HERE. 
 // shop/model/ is where we will put the data and rules, not stuff w/ screens/clicks 
 // shop/model/Upgrade.java is where the idea of the upgrades will be eg. cost, effect, name, description, etc. 
-public class Upgrade { 
+public class Upgrade {
+
     // data for the upgrades 
-    private String name; 
-    private int cost; 
-    private String description; 
-    private int bonusAmount; 
-    // obvious 
-    public Upgrade(String name, int cost, String description, int bonusAmount) { 
-        this.name = name; 
-        this.cost = cost; 
-        this.description = description; 
-        this.bonusAmount = bonusAmount; 
-        } 
-    // getters for the data 
-    public String getName() { 
-        return name; 
-    } 
-    public int getCost() { 
-        return cost; 
-    } 
-    public String getDescription() { 
-        return description; 
-    } 
-    public int getBonusAmount() { 
-        return bonusAmount; 
-    } 
+    private String name;
+    private int cost;
+    private String description;
+
+    private String statAffected;      // "speed", "power", etc.
+    private double bonusAmount;       // 1 or 1.05
+    private boolean percentageBased;  // false = +1, true = ×1.05
+    private boolean oneTimeUse;
+
+    public Upgrade(
+            String name,
+            int cost,
+            String description,
+            String statAffected,
+            double bonusAmount,
+            boolean percentageBased,
+            boolean oneTimeUse
+    ) {
+        this.name = name;
+        this.cost = cost;
+        this.description = description;
+        this.statAffected = statAffected;
+        this.bonusAmount = bonusAmount;
+        this.percentageBased = percentageBased;
+        this.oneTimeUse = oneTimeUse;
+    }
+
+    // getters only
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatAffected() {
+        return statAffected;
+    }
+
+    public double getBonusAmount() {
+        return bonusAmount;
+    }
+
+    public boolean isPercentageBased() {
+        return percentageBased;
+    }
+
+    public boolean isOneTimeUse() {
+        return oneTimeUse;
+    }
 }
