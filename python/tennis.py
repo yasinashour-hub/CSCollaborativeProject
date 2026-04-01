904fcc63b7b793acd726c1f596f1586378820534
anvilongview <anvi.upadrashta@long-view.com>
2026-04-01 12:35:54 -0500

    Update tennis.py

--- python/tennis.py
+++ python/tennis.py
@@ -262,8 +262,8 @@ score2 = 0
 
 # Music Set Up
 pygame.mixer.music.load("python/assets/strategy_twice.mp3")
-pygame.mixer.music.play()
-pygame.mixer.music.set_volume(0.8)
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(1.0)
 
 # Pre-Main Game Screens
 show_home_screen() # Call wait screen function before main game untill quitted
